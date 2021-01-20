894fcc3a54b20aa2f1bf320a64ff586cd2ef56d7
Mac <mac@MacBook-Air-Mac.local>
2021-01-20 19:06:57 +0200

    HomePage carousel refactoring

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -4,10 +4,30 @@ import { Container, Row } from 'react-bootstrap';
 import ProductsCarousel from './components/ProductsCarousel';
 import TextContent from './components/TextContent';
 
-const HomePage = () => {  
+const HomePage = () => {
+
+  const carouselItems = [
+    {
+      category: 'mac',
+      src: 'https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fmac-img.jpg?alt=media&token=7f174508-19c0-4b6b-8bf3-0344598880c1',
+    },
+    {
+      category: 'iphone',
+      src: 'https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fiphone-img.jpg?alt=media&token=e6850ca4-4c6c-4d56-9cbe-ed82e53ad022',
+    },
+    {
+      category: 'ipad',
+      src: 'https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fipad-img.jpg?alt=media&token=1839bde9-a774-4a59-ab48-fa74e9278701',
+    },
+    {
+      category: 'apple-watch',
+      src: 'https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fwatch-img.jpg?alt=media&token=30d1ae29-4a9d-436b-88d9-67eeb9022efd',
+    },
+  ];
+
   return (
     <>
-      <ProductsCarousel />
+      <ProductsCarousel carouselItems={carouselItems} />
       <Container>
         <Row>
           <TextContent />
--- src/containers/HomePage/components/ProductsCarousel/ProductsCarousel.jsx
+++ src/containers/HomePage/components/ProductsCarousel/ProductsCarousel.jsx
@@ -2,48 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom'; 
 import { Carousel } from 'react-bootstrap';
 
-// TODO: make reusable + needs refactoring
-const ProductsCarousel = () => (
+// TODO: needs refactoring (image loading)
+
+const ProductsCarousel = ({ carouselItems }) => (
   <Carousel indicators={false}>
-    <Carousel.Item>
-      <Link to="/products/mac">
-        <img
-          className="d-block w-100"
-          src="https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fmac-img.jpg?alt=media&token=7f174508-19c0-4b6b-8bf3-0344598880c1"
-          alt="mac"
-        />
-      </Link>
-    </Carousel.Item>
-
-    <Carousel.Item>
-      <Link to="/products/iphone">
-        <img
-          className="d-block w-100"
-          src="https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fiphone-img.jpg?alt=media&token=e6850ca4-4c6c-4d56-9cbe-ed82e53ad022"
-          alt="iphone"
-        />
-      </Link>
-    </Carousel.Item>
-
-    <Carousel.Item>
-      <Link to="/products/ipad">
-        <img
-          className="d-block w-100"
-          src="https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fipad-img.jpg?alt=media&token=1839bde9-a774-4a59-ab48-fa74e9278701"
-          alt="ipad"
-        />
-      </Link>
-    </Carousel.Item>
-
-    <Carousel.Item>
-      <Link to="/products/apple-watch">
-        <img
-          className="d-block w-100"
-          src="https://firebasestorage.googleapis.com/v0/b/istore-react.appspot.com/o/homepage%2Fcarousel%2Fwatch-img.jpg?alt=media&token=30d1ae29-4a9d-436b-88d9-67eeb9022efd"
-          alt="apple-watch"
-        />
-      </Link>
-    </Carousel.Item>
+    {carouselItems.map(carouselItem => (
+      <Carousel.Item key={carouselItem?.category}>
+        <Link to={`/products/${carouselItem?.category}`}>
+          <img
+            className="d-block w-100"
+            src={carouselItem?.src}
+            alt={carouselItem?.category}
+          />
+        </Link>
+      </Carousel.Item>
+    ))}
   </Carousel>
 );
 
